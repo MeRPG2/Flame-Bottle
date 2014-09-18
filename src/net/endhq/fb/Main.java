69065192d06ec3c28d89e297e3e42a55b2480fb1
MeRPG <jaxon007@verizon.net>
2014-09-18 07:19:00 -0400

    a

--- src/net/endhq/fb/Main.java
+++ src/net/endhq/fb/Main.java
@@ -2,11 +2,14 @@ package net.endhq.fb;
 
 import net.endhq.isb.ItemStackBuilder;
 
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
@@ -25,6 +28,6 @@ public class Main extends JavaPlugin implements Listener {
 	
 	@EventHandler
 	public void onPlayerInteractEvent(PlayerInteractEvent e) {
-		e.getPlayer().getWorld().dropItem(e.getPlayer().getLocation(), new ItemStackBuilder(Material))
+		Item i = e.getPlayer().getWorld().dropItem(e.getPlayer().getLocation(), new ItemStack(Material.getMaterial(2261), 1));
 	}
 }
